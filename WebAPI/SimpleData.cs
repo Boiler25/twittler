a3654498ae1b7a3e61b911af8bcb30af86162a51
boiler25 <nikitosbykov25@gmail.com>
2018-11-09 11:58:51 +0300

    Added db initializer

---
+++ WebAPI/SimpleData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI
+{
+    public class SimpleData
+    {
+        public static void Initialize(NoteContext context)
+        {
+            if (!context.Notes.Any())
+            {
+                context.Notes.AddRange(
+                    new Note
+                    {
+                        Text = "First Note"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
