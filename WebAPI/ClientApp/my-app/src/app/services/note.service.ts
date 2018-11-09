e23ceaaec775e2f21a6a22e0da7e91feefd82473
boiler25 <nikitosbykov25@gmail.com>
2018-11-09 12:21:49 +0300

    Added service

--- WebAPI/ClientApp/my-app/src/app/services/note.service.ts
+++ WebAPI/ClientApp/my-app/src/app/services/note.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Note } from '../models/note';
+import { Note } from './models/note';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
-constructor() { }
+constructor(private httpClient: HttpClient) { }
+private url = 'http://localhost:50947';
+public getNotes(id?: number): Promise<Note> {
 
+  let params = new HttpParams();
+  if (id) {
+    params = params.append('id', id.toString());
+
+  return this.httpClient.get<Note>(this.url, { params }).toPromise();
+  }
+}
 }
